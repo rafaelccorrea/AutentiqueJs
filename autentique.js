8bf90e13fae729f016ba4ae86f43133bce1560ac
Rafael Correa <68115778+rafaelccorrea@users.noreply.github.com>
2021-12-27 12:02:36 -0300

    Update autentique.js

--- autentique.js
+++ autentique.js
@@ -1,130 +1,119 @@
 const { default: api} = require('autentique-v2-nodejs')
-const { criarServico } = require('../serviceHelper')
 
  api.token = process.env.AUTENTIQUE_TOKEN;
  api.url = process.env.URL;
 
- const token = {token: api.token}
+ const criarDocumento = async (dadoscliente, url) => {
 
-exports.createAutentique = criarServico(
-    async (conexao, dados) => {
-        //Passar atributos como um objeto
-        const attributes = {
-            document: {
-                name: 'Dev-Teste'
-            },
-            signers: [
-                {
-                    email: 'rafaelgustavocorrea@gmail.com',
-                    action: 'SIGN'
-                }
-            ],
-            file: 'https://cucafresca.blob.core.windows.net/cucafresca-apps-dev/contrato-modelo-web/1639503660539.pdf'
-        };
+    const attributes = {
+        document: {
+            name: 'Contrato' + ' - ' + dadoscliente.razaosocial
+        },
+        signers: [
+            {
+                email: dadoscliente.email,
+                action: 'SIGN'
+            }
+        ],
+        file: url
+    };
+     return await api.document.create(attributes);
 
-        const signature = await api.document.create(attributes).then((response) => {
-            return response
-        }).catch(err => {
-            const error = err.response.data
-        });
+};
 
-        const result = signature
+const listarDocumento = async (documento) => {
 
-        return { result };
-    },
-    {
-        onSucesso: 'Documento criado com sucesso!',
-        onErroInterno: 'Erro ao criar Documento!'
-    }
-)
+     const documentId = { documentId: documento }
+     const contrato =  await api.document.listById(documentId);
+     return { contrato };
 
-exports.listAutentiqueId = criarServico (
+};
 
-    async (conexao, dados) => {
-        //Id Principal do documento
-        const documentId = {
-            documentId: 'f1b30b8c6c5df95728e2f0e764b7586a4a7567f3034146495'
+const listarTodosDocumento = async () => {
+
+    const contrato =  await api.document.listAll(1);
+    return { contrato };
+
+};
+
+const assinarDocumento = async (documento) => {
+
+    const documentId = { documentId: documento }
+    const contrato = await api.document.signById(documentId)
+    return { contrato };
+
+};
+
+const criarPasta = async (dados) => {
+    const pasta = {
+        folder: {
+            name: dados.id_cliente + ' - ' + dados.chave
         }
-
-        const signature = await api.document.listById(documentId).then(res => {
-            return res
-        }).catch(err => {
-            const error = err.response.data
-        });
-        const result = signature
-        return { result };
-    },
-    {
-        onSucesso: 'Documento consultado com sucesso!',
-        onErroInterno: 'Erro ao consultar Documento!'
-    }
-)
-
-
-exports.deleteDocument = criarServico (
-
-    async (conexao, dados) => {
-
-        const documentId = {
-            documentId: 'f1b30b8c6c5df95728e2f0e764b7586a4a7567f3034146495'
-        }
-
-        const signature = await api.document.deleteById(documentId).then(res => {
-            return res
-        }).catch(err => {
-            const error = err
-        });
-        console.log(signature)
-        return { signature };
-    },
-    {
-        onSucesso: 'Documento deletado com sucesso!',
-        onErroInterno: 'Erro ao deletar Documento!'
-    }
-)
-
-exports.signDocument = criarServico (
-
-    async (conexao, dados) => {
-
-        //Para assinar Documento necessita que o Token seja o do cliente que ira assinar o documento.
-        const documentId = {
-            documentId: '538887b01d4c551558622cf042be8a0d28d3354f54c93d916'
-        }
-
-        const signature = await api.document.signById(documentId).then(res => {
-            return res
-        }).catch(err => {
-            const error = err
-        });
-
-        const result = signature
-        return { result };
-    },
-    {
-        onSucesso: 'Documento assinado com sucesso!',
-        onErroInterno: 'Erro ao assinar Documento!'
     }
 
-)
+    return await api.folder.create(pasta);
 
-exports.listAll = criarServico (
+}
 
-    //Passar quantidade de paginas como parametro. (limit: 60)
-    async (conexao, dados) => {
+const movendoPasta = async (arquivo, assinatura) => {
 
-        const signature = await api.document.listAll(1).then((response)=> {
-            return response
-        }).catch(err => {
-            return err
-        });
-        const result = signature
+    const pasta = {
+            documentId: assinatura.data.createDocument.id,
+            folderId: arquivo.data.createFolder.id
+    }
+    
+    return await api.folder.moveDocumentById(pasta);
+}
 
-        return { result };
-    },
-    {
-        onSucesso: 'Documentos Listados com sucesso!',
-        onErroInterno: 'Erro ao listar Documentos!'
+const listarTodasPastas = async () => {
+
+    const pasta = await api.folder.listAll(1);
+    const result = pasta.data.folders
+    return { result };
+
+};
+
+//Inativo
+const ListPasta = async (folder) => {
+    const folderId = {
+        folderId: folder.data.createFolder.id || folder
+    }
+    const pasta = await api.folder.listById(folderId);
+    return { pasta };
+}
+
+
+const deleteDocument = async (documento) => {
+    
+    const deleteDocument = {
+        documentId: documento.id_documento
     }
 
-)
+    const contrato = await api.document.deleteById(deleteDocument)
+    return { contrato };
+    
+}
+
+const deleteFolder = async (folder) => {
+    
+    const deleteFolder = {
+        folderId: folder.id_pasta
+    }
+
+    const pasta = await api.folder.deleteById(deleteFolder)
+    return { pasta };
+    
+}
+
+module.exports = {
+    criarDocumento,
+    listarDocumento,
+    listarTodosDocumento,
+    assinarDocumento,
+    criarPasta,
+    movendoPasta,
+    ListPasta,
+    deleteDocument,
+    listarTodasPastas,
+    deleteFolder
+}
